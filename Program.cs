d1459f4e3e788627a0a7cedd83f97467601c8d53
2021-0082 <2021-0082@uteco.edu.do>
2025-05-21 06:56:31 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+	private static void Main(string[] args)
+	{
+		Console.WriteLine("Por favor, ingresa tu fecha de nacimiento (yyyy/mm/dd):");
+		string input = Console.ReadLine();
+
+		if (DateTime.TryParse(input, out DateTime fechaNacimiento))
+		{
+			string diaSemana = fechaNacimiento.DayOfWeek.ToString();
+			Console.WriteLine($"Naciste un {diaSemana}.");
+		}
+		else
+		{
+			Console.WriteLine("Formato de fecha incorrecto. Por favor, utiliza el formato yyyy/mm/dd.");
+		}
+	}
+}
